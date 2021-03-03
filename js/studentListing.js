c620e2f50db5bd566f0b4ec6cfbfc5807bc0b80b
samfrolick <samfrolick@gmail.com>
2021-03-02 21:14:13 -0600

    Indented dynamic JS

--- js/studentListing.js
+++ js/studentListing.js
@@ -10,24 +10,24 @@ async function loadStudents() {
                 var studentCardGrid = document.getElementById("studentCardGrid");
                 studentCardGrid.innerHTML += (
                     '<div class="col">' +
-                    '<div class="card mb-3" style="max-width: 540px;">' +
-                    '<div class="row g-0">' +
-                    '<div class="col-md-4">' +
-                    '<object class="card-img-top" data="img/' + result["username"] + '.jpg" type="image/jpg">' +
-                    '<img class="card-img-top" src="img/image.jpg" />' +
-                    '</object>' +
+                        '<div class="card mb-3" style="max-width: 540px;">' +
+                            '<div class="row g-0">' +
+                                '<div class="col-md-4">' +
+                                    '<object class="card-img-top" data="img/' + result["username"] + '.jpg" type="image/jpg">' +
+                                        '<img class="card-img-top" src="img/image.jpg" />' +
+                                    '</object>' +
                     //'<img class="card-img-top" src="img/1232956.jpg" alt="...">' +
-                    '</div>' +
-                    '<div class="col-md-8">' +
-                    '<div class="card-body">' +
-                    '<h5 class="card-title">' + result["username"] + '</h5>' +
-                    '<p class="card-text">' + result["email"] + '</p>' +
-                    '<p class="card-text">' + result["age"] + '</p>' +
-                    '<p class="card-text"><small class="text-muted">' + result["walletAddress"] + '</small></p>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
+                                '</div>' +
+                                '<div class="col-md-8">' +
+                                    '<div class="card-body">' +
+                                        '<h5 class="card-title">' + result["username"] + '</h5>' +
+                                        '<p class="card-text">' + result["email"] + '</p>' +
+                                        '<p class="card-text">' + result["age"] + '</p>' +
+                                        '<p class="card-text"><small class="text-muted">' + result["walletAddress"] + '</small></p>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
                     '</div>'
                 )
             }
